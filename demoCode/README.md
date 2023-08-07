e38c82c3424e01415fa8d33463c6a6654bc65e73
Kara Cavanaugh <kara.cavanaugh@fullstackacademy.com>
2023-08-07 17:18:23 -0400

    initial commit

---
+++ demoCode/README.md
@@ -0,0 +1,10 @@
+# Demo Code
+
+Just some simple examples of passing props between components
+
+1. Run `npm install`
+2. Run `npm run build`
+3. Run `npm run preview` to start the Vite app
+
+The code you want to review is in src > assets: App.jsx and Pizza.jsx
+- There is commented code in App.jsx - if you uncomment it, you'll be able to see a button and practice useState to change the button count. That has nothing to do with the Pizza part, it was just useState review
---
+++ demoCode/src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import Pizza from "./Pizza.jsx";
+
+function App() {
+ // const [count, setCount] = useState(0);
+  const toppings = ['pepperoni', 'olives', 'honey', 'pineapple'];
+  console.log(toppings);
+  return (
+    <div className="card">
+      {/* <Button number={count} setNumber={setCount} /> */}
+      <Pizza type={toppings} size="medium"/>
+    </div>
+  );
+}
+
+// function Button({ number, setNumber }) {
+//   return (
+//     <button onClick={() => setNumber((number) => number + 1)}>
+//       count is {number}
+//     </button>
+//   );
+// }
+
+export default App;
